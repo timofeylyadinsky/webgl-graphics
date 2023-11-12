45a7757159ac6f8e0407475bbe1904e33c5dbe25
Timofey Lyadinsky <tima.led12@gmail.com>
2023-11-12 19:42:10 +0300

    feat: start 1

--- lab1/script.js
+++ lab1/script.js
@@ -1,4 +1,4 @@
-var gl; // глобальная переменная для контекста WebGL
+let gl; // глобальная переменная для контекста WebGL
 
 "use strict";
 
@@ -30,19 +30,102 @@ function createProgram(gl, vertexShader, fragmentShader) {
 
 
 function start() {
-  var canvas = document.getElementById("glcanvas");
+  let canvas = document.getElementById("glcanvas");
 
   gl = initWebGL(canvas); // инициализация контекста GL
 
   // продолжать только если WebGL доступен и работает
 
   if (gl) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0); // установить в качестве цвета очистки буфера цвета чёрный, полная непрозрачность
+    gl.clearColor(1.0, 0.0, 0.0, 1.0); // установить в качестве цвета очистки буфера цвета чёрный, полная непрозрачность
     gl.enable(gl.DEPTH_TEST); // включает использование буфера глубины
     gl.depthFunc(gl.LEQUAL); // определяет работу буфера глубины: более ближние объекты перекрывают дальние
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // очистить буфер цвета и буфер глубины.
+  } else {
+    return;
   }
 
+
+  // Get the strings for our GLSL shaders
+  let vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+  let fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
+ 
+   // create GLSL shaders, upload the GLSL source, compile the shaders
+  let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+ 
+  let program = createProgram(gl, vertexShader, fragmentShader);
+
+   // look up where the vertex data needs to go.
+   var positionLocation = gl.getAttribLocation(program, "a_position");
+
+   // lookup uniforms
+   var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+   var colorLocation = gl.getUniformLocation(program, "u_color");
+   var translationLocation = gl.getUniformLocation(program, "u_translation");
+
+  // Create a buffer and put three 2d clip space points in it
+  let positionBuffer = gl.createBuffer();
+
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(choords),
+    gl.STATIC_DRAW
+  )
+
+  var color = [Math.random(), Math.random(), Math.random(), 1];
+  var translation = [0, 0];
+
+  drawScene();
+
+  function drawScene() {
+    //webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Clear the canvas.
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the attribute
+    gl.enableVertexAttribArray(positionLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionLocation, size, type, normalize, stride, offset);
+
+    // set the resolution
+    gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+
+    // set the color
+    gl.uniform4fv(colorLocation, color);
+
+    // Set the translation.
+    gl.uniform2fv(translationLocation, translation);
+
+    // Draw the geometry.
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 18;  // 6 triangles in the 'F', 3 points per triangle
+    gl.drawArrays(primitiveType, offset, count);
+  }
+
+
+  
   console.log("print")
 }
 
@@ -62,3 +145,30 @@ function initWebGL(canvas) {
   
     return gl;
   }
+
+
+  let choords = [
+    // left column
+    0, 0,
+    30, 0,
+    0, 150,
+    0, 150,
+    30, 0,
+    30, 150,
+
+    // top rung
+    30, 0,
+    100, 0,
+    30, 30,
+    30, 30,
+    100, 0,
+    100, 30,
+
+    // middle rung
+    30, 60,
+    67, 60,
+    30, 90,
+    30, 90,
+    67, 60,
+    67, 90,
+]
